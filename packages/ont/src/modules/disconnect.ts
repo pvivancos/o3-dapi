3e722dd94fb321737dbae84b9c4042311ea2b35e
Nick Fujita <nickfujita@gmail.com>
2018-12-10 12:04:05 +0900

    ont version bump, update disconnect return value

--- packages/ont/src/modules/disconnect.ts
+++ packages/ont/src/modules/disconnect.ts
@@ -1,7 +1,7 @@
 import { sendMessage } from '../messaging';
 import { Command } from '../constants';
 
-export function disconnect(): Promise<void> {
+export function disconnect(): Promise<boolean> {
   return sendMessage({
     command: Command.disconnect,
   });
