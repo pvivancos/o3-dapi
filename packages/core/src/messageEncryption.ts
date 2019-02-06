09dea097e95fe7951b792f3742c430693597ba55
Nick Fujita <nickfujita@gmail.com>
2019-02-06 15:31:22 +0900

    updated encryption algo for messaging to use initial vector

--- packages/core/src/messageEncryption.ts
+++ packages/core/src/messageEncryption.ts
@@ -1,4 +1,6 @@
-import { createCipher, createDecipher } from 'browserify-cipher';
+import { Buffer } from 'buffer';
+import { randomFillSync } from 'randomfill';
+import { createCipheriv, createDecipheriv } from 'browserify-cipher';
 import createECDH from 'create-ecdh';
 const AES256 = 'aes256';
 const UTF8 = 'utf8';
@@ -28,28 +30,36 @@ export default class MessageEncryption {
       message,
       nonce: Date.now() + Math.floor(Math.random() * 100000000000000000),
     };
-    const cipher = createCipher(AES256, this.shared);
-    return cipher.update(JSON.stringify(data), UTF8, HEX) + cipher.final(HEX);
+    const iv = Buffer.alloc(16);
+    randomFillSync(iv);
+    const cipher = createCipheriv(AES256, this.shared, iv);
+    const encrypted = cipher.update(JSON.stringify(data), UTF8, HEX) + cipher.final(HEX);
+    return `${iv.toString('hex')}:${encrypted.toString()}`;
   }
 
   decrypt(input: string): {message, error?: string} {
-      try {
-        const decipher = createDecipher(AES256, this.shared);
-        const data = JSON.parse(decipher.update(input, HEX, UTF8) + decipher.final(UTF8));
-        if (!data || !data.nonce || this.nonceMap[data.nonce]) {
-          return {
-            message: data.message,
-            error: 'Nonce from provider was already used, message invalid.',
-          };
-        }
+    console.log('decrypt', input);
+    try {
+      const textParts = input.split(':');
+      const iv = Buffer.from(textParts.shift(), 'hex');
+      const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+      const decipher = createDecipheriv(AES256, this.shared, iv);
+      const data = JSON.parse(decipher.update(encryptedText, HEX, UTF8) + decipher.final(UTF8));
 
-        this.nonceMap[data.nonce] = true;
-        return { message: data.message };
-      } catch (err) {
+      if (!data || !data.nonce || this.nonceMap[data.nonce]) {
         return {
-          message: input,
-          error: 'There was an error decrypting the message.',
+          message: data.message,
+          error: 'Nonce from provider was already used, message invalid.',
         };
       }
+
+      this.nonceMap[data.nonce] = true;
+      return { message: data.message };
+    } catch (err) {
+      return {
+        message: input,
+        error: 'There was an error decrypting the message.',
+      };
     }
+  }
 }
