888b9796aa8cbf88644f111c0af65a7f5ab5b227
Nick Fujita <nickfujita@gmail.com>
2019-03-01 17:40:43 +0900

    wip, provide alternative encryption algo

--- packages/core/lib/messageEncryption.d.ts
+++ packages/core/lib/messageEncryption.d.ts
@@ -1,8 +1,10 @@
+export declare const AES128GCM = "aes-128-gcm";
 export default class MessageEncryption {
     private ec;
     private key;
     private shared;
     private nonceMap;
+    private cipherAlgorithm;
     getPublicKey(): any;
     setSharedKey(key: any): void;
     hasSharedKey(): boolean;
@@ -11,5 +13,7 @@ export default class MessageEncryption {
         message: any;
         error?: string;
     };
+    setCipherAlgorithOverride(algorithm: string): void;
+    private _getSharedKey;
 }
 //# sourceMappingURL=messageEncryption.d.ts.map
--- packages/core/src/messageEncryption.ts
+++ packages/core/src/messageEncryption.ts
@@ -5,6 +5,7 @@ import createECDH from 'create-ecdh';
 const AES256 = 'aes256';
 const UTF8 = 'utf8';
 const HEX = 'hex';
+export const AES128GCM = 'aes-128-gcm';
 
 export default class MessageEncryption {
 
@@ -12,6 +13,7 @@ export default class MessageEncryption {
   private key = this.ec.generateKeys();
   private shared;
   private nonceMap = {};
+  private cipherAlgorithm = AES256;
 
   getPublicKey() {
     return this.key;
@@ -32,7 +34,7 @@ export default class MessageEncryption {
     };
     const iv = Buffer.alloc(16);
     randomFillSync(iv);
-    const cipher = createCipheriv(AES256, this.shared, iv);
+    const cipher = createCipheriv(this.cipherAlgorithm, this._getSharedKey(), iv);
     const encrypted = cipher.update(JSON.stringify(data), UTF8, HEX) + cipher.final(HEX);
     return `${iv.toString('hex')}:${encrypted.toString()}`;
   }
@@ -42,7 +44,7 @@ export default class MessageEncryption {
       const textParts = input.split(':');
       const iv = Buffer.from(textParts.shift(), 'hex');
       const encryptedText = Buffer.from(textParts.join(':'), 'hex');
-      const decipher = createDecipheriv(AES256, this.shared, iv);
+      const decipher = createDecipheriv(this.cipherAlgorithm, this._getSharedKey(), iv);
       const data = JSON.parse(decipher.update(encryptedText, HEX, UTF8) + decipher.final(UTF8));
 
       if (!data || !data.nonce || this.nonceMap[data.nonce]) {
@@ -60,5 +62,16 @@ export default class MessageEncryption {
         error: 'There was an error decrypting the message.',
       };
     }
+  }
+
+  setCipherAlgorithOverride(algorithm: string) {
+    this.cipherAlgorithm = algorithm;
+  }
+
+  private _getSharedKey() {
+    if (this.cipherAlgorithm === AES128GCM) {
+      return this.shared.slice(0, 16);
+    }
+    return this.shared;
   }
 }
