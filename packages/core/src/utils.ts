1adbe34f91f989c1b1503d6db4cafec3c44849d2
Nick Fujita <nickfujita@gmail.com>
2019-03-14 13:03:59 +0900

    got address scripthash methods working

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,4 +1,7 @@
+declare const Buffer: any;
 import base58 from 'bs58';
+import SHA256 from 'crypto-js/sha256';
+import hexEncoding from 'crypto-js/enc-hex';
 
 export function hex2str(input: string): string {
   return ab2str(hex2ab(input));
@@ -12,9 +15,9 @@ export function hex2int(input: string): number {
   return parseInt(reverseHex(input), 16);
 }
 
-export function num2hex(input: number): string {
-  const h = input.toString(16);
-  return h.length % 2 ? '0' + h : h;
+export function int2hex(input: number): string {
+  const h = Number(input).toString(16);
+  return reverseHex(h.length % 2 ? '0' + h : h);
 }
 
 export function reverseHex(input: string): string {
@@ -27,15 +30,23 @@ export function reverseHex(input: string): string {
 
 export function address2scriptHash(input: string): string {
   const hash = ab2hex(base58.decode(input));
-  return reverseHex(hash.substr(2, 40));
+  return hash.substr(2, 40);
 }
 
 export function scriptHash2address(input: string): string {
-  // input = reverseHex(input);
-  // const shaChecksum = hash256('17' + input).substr(0, 8);
-  // return base58.encode(
-  //   Buffer.from('17' + input + shaChecksum, 'hex')
-  // );
+  const ADDR_VERSION = '17';
+  input = input.substr(0, 40);
+  const firstSha = sha256(ADDR_VERSION + input);
+  const secondSha = sha256(firstSha);
+  const shaChecksum = secondSha.substr(0, 8);
+  const arrayBuffer = hex2ab(ADDR_VERSION + input + shaChecksum);
+  return base58.encode(new Buffer(arrayBuffer, 'hex'));
+}
+
+function sha256(data: string) {
+  const hex = hexEncoding.parse(data);
+  const sha = SHA256(hex).toString();
+  return sha;
 }
 
 function ab2str(input: ArrayBuffer | ArrayLike<number>): string {
@@ -61,11 +72,11 @@ function ab2hex(input: ArrayBuffer | ArrayLike<number>): string {
   return result;
 }
 
-function hex2ab(input: string): Uint8Array {
+function hex2ab(input: string): number[] {
   const result = [];
   while (input.length >= 2) {
     result.push(parseInt(input.substring(0, 2), 16));
     input = input.substring(2, input.length);
   }
-  return new Uint8Array(result);
+  return result;
 }
