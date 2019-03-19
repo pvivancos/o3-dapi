dc53b938d6aa546ed5f3be02dcdeaedb48760af7
Apisit Toompakdee <apmotive@gmail.com>
2019-03-19 12:26:49 +0900

    added deploy page

--- packages/neo/test/functions.js
+++ packages/neo/test/functions.js
@@ -52,7 +52,6 @@ var app = new Vue({
 	}
 })
 
-
 const networksEle = document.getElementById("networks");
 
 
@@ -205,39 +204,3 @@ function verifyMessage(inputElement, resultElem) {
 		document.getElementById(resultElem).innerHTML = 'invalid JSON input';
 	}
 }
-
-
-function deploy() {
-
-}
-
-//utilties section
-function hex2str(inputElement, resultElem){
-	document.getElementById(resultElem).innerHTML =  o3dapi.utils.hex2str(document.getElementById(inputElement).value)
-}
-
-function str2hex(inputElement, resultElem){
-	document.getElementById(resultElem).innerHTML =  o3dapi.utils.str2hex(document.getElementById(inputElement).value)
-}
-
-
-function hex2int(inputElement, resultElem){
-	document.getElementById(resultElem).innerHTML =  o3dapi.utils.hex2int(document.getElementById(inputElement).value)
-}
-
-function int2hex(inputElement, resultElem){
-	document.getElementById(resultElem).innerHTML =  o3dapi.utils.int2hex(document.getElementById(inputElement).value)
-}
-
-function reverseHex(inputElement, resultElem){
-	document.getElementById(resultElem).innerHTML =  o3dapi.utils.reverseHex(document.getElementById(inputElement).value)
-}
-
-function address2scripthash(inputElement, resultElem){
-	document.getElementById(resultElem).innerHTML =  o3dapi.utils.address2scriptHash(document.getElementById(inputElement).value)
-}
-
-function scripthash2address(inputElement, resultElem){
-	document.getElementById(resultElem).innerHTML =  o3dapi.utils.scriptHash2address(document.getElementById(inputElement).value)
-}
-
